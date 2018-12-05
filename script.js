7565032dbead213a1a43992d417fc6690af0b1b8
johnathantolento <42359128+johnathantolento@users.noreply.github.com>
2018-12-05 14:58:08 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#button2").click(function() {
+    $("#what").fadeToggle();
+});
+$("#button1").click(function() {
+    $("#when").slideToggle();
+});
+$("#button3").click(function() {
+    $("#where").toggle();
+});
